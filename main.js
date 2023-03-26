9e6c08ff85f89aa1d777848ea48b870ec285d93e
joshuaopel <joshuaopel@gmail.com>
2023-03-26 16:31:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ const playerNames = ["Alice", "Bob", "Charlie", "David"];
 
 let gameInProgress = false;
 let gamePaused = false;
+
+let players = [];
+
 let gameTimer;
 let gameTime = 0;
 const gameTimeDisplay = document.getElementById("gameTime");
@@ -28,58 +31,6 @@ function updateGameTimeDisplay() {
     const minutes = Math.floor(gameTime / 60);
     const seconds = gameTime % 60;
     gameTimeDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-}
-
-
-let players = [];
-
-function createPlayer(name) {
-    const player = {
-        name: name,
-        playing: false,
-        position: "",
-        playTime: 0,
-        benchTime: 0,
-        benchStart: null,
-    };
-    return player;
-}
-
-function updateGameHistory() {
-    const savedGames = JSON.parse(localStorage.getItem("gameHistory")) || [];
-    gameHistory.innerHTML = "";
-    savedGames.forEach((game, index) => {
-        const gameDiv = document.createElement("div");
-        gameDiv.classList.add("game");
-        gameDiv.innerHTML = `<h3>Game ${index + 1}</h3>`;
-        game.players.forEach((player) => {
-            const playerDiv = document.createElement("div");
-            playerDiv.innerHTML = `${player.name}: ${player.playTime} seconds played as ${player.position}`;
-            gameDiv.appendChild(playerDiv);
-        });
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Delete Game";
-        deleteBtn.addEventListener("click", () => {
-            savedGames.splice(index, 1);
-            localStorage.setItem("gameHistory", JSON.stringify(savedGames));
-            updateGameHistory();
-        });
-        gameDiv.appendChild(deleteBtn);
-        gameHistory.appendChild(gameDiv);
-    });
-}
-
-function updatePlayerDisplay(player, playerDiv) {
-    playerDiv.innerHTML = `
-        <h3>${player.name}</h3>
-        <button class="togglePlay" ${player.playing || !gameInProgress ? "disabled" : ""}>Play</button>
-        <select class="position" ${player.playing || !gameInProgress ? "disabled" : ""}>
-            ${positions.map((pos) => `<option ${player.position === pos ? "selected" : ""}>${pos}</option>`).join("")}
-        </select>
-        <button class="toggleBench" ${!player.playing || !gameInProgress ? "disabled" : ""}>${player.benchStart ? "Unbench" : "Bench"}</button>
-        <p>Play Time: ${player.playTime} seconds</p>
-        <p>Bench Time: ${player.benchTime} seconds</p>
-    `;
 }
 
 function updatePlayerDisplay(player, playerDiv) {
@@ -117,6 +68,24 @@ function createPlayerDiv(player) {
         player.position = e.target.value;
     });
 
+    const benchBtn = document.createElement("button");
+    benchBtn.classList.add("toggleBench");
+    benchBtn.textContent = player.benchStart ? "Unbench" : "Bench";
+    benchBtn.addEventListener("click", () => {
+        if (player.benchStart) {
+            player.benchStart = null;
+        } else {
+            player.benchStart = new Date();
+        }
+        updatePlayerDisplay(player, playerDiv);
+    });
+
+    const playTime = document.createElement("p");
+    playTime.classList.add("playTime");
+
+    const benchTime = document.createElement("p");
+    benchTime.classList.add("benchTime");
+
     playerDiv.appendChild(playBtn);
     playerDiv.appendChild(positionSelect);
     playerDiv.appendChild(benchBtn);
@@ -126,50 +95,56 @@ function createPlayerDiv(player) {
     return playerDiv;
 }
 
+function createPlayer(name) {
+    return {
+        name: name,
+        playing: false,
+        position: positions[0],
+        playTime: 0,
+        benchTime: 0,
+        benchStart: null,
+        benchPauseStart: null,
+    };
+}
+
+playerNames.forEach((name) => {
+    const player = createPlayer(name);
+    players.push(player);
+    playersDiv.appendChild(createPlayerDiv(player));
+});
+
 loginBtn.addEventListener("click", () => {
     if (passwordInput.value === hardcodedPassword) {
         login.style.display = "none";
         app.style.display = "block";
-        passwordInput.value = "";
-    } else {
-        alert("Incorrect password!");
     }
 });
 
-
 startGame.addEventListener("click", () => {
     gameInProgress = true;
-    gamePaused = false;
     startGame.disabled = true;
     pauseGame.disabled = false;
     endGame.disabled = false;
-
-    players.forEach((player) => {
-        if (player.playing) {
-            player.benchStart = new Date();
-        }
-    });
-
-    setInterval(() => {
-        if (!gameInProgress || gamePaused) return;
-
-        gameTime++;
-        updateGameTimeDisplay();
-
-        players.forEach((player, index) => {
-            if (player.playing && !player.benchStart) {
-                player.playTime++;
-            } else if (player.playing && player.benchStart) {
-                player.benchTime++;
-            }
-            updatePlayerDisplay(player, playersDiv.children[index]);
-        });
-    }, 1000);
 });
 
 pauseGame.addEventListener("click", () => {
     gamePaused = !gamePaused;
     pauseGame.textContent = gamePaused ? "Resume Game" : "Pause Game";
+
+    if (gamePaused) {
+        players.forEach((player) => {
+            if (player.playing && player.benchStart) {
+                player.benchPauseStart = new Date();
+            }
+        });
+    } else {
+        players.forEach((player) => {
+            if (player.playing && player.benchPauseStart) {
+                player.benchStart = new Date(new Date() - (player.benchPauseStart - player.benchStart));
+                player.benchPauseStart = null;
+            }
+        });
+    }
 });
 
 endGame.addEventListener("click", () => {
@@ -183,29 +158,31 @@ endGame.addEventListener("click", () => {
 });
 
 saveGame.addEventListener("click", () => {
-    const gameHistoryData = JSON.parse(localStorage.getItem("gameHistory")) || [];
-    const newGameData = {
-        players: JSON.parse(JSON.stringify(players)),
+    const savedGame = {
+        date: new Date(),
+        players: players.map((player) => ({ ...player })),
+        gameTime: gameTime,
     };
-    gameHistoryData.push(newGameData);
-    localStorage.setItem("gameHistory", JSON.stringify(gameHistoryData));
-    updateGameHistory();
 
-    players = playerNames.map(createPlayer);
-    playersDiv.innerHTML = "";
-    players.forEach((player) => {
-        const playerDiv = createPlayerDiv(player);
-        playersDiv.appendChild(playerDiv);
-    });
+    const savedGameDiv = document.createElement("div");
+    savedGameDiv.classList.add("savedGame");
+    savedGameDiv.textContent = `Game on ${savedGame.date.toLocaleString()}: ${savedGame.gameTime} seconds`;
 
-    saveGame.disabled = true;
+    gameHistory.appendChild(savedGameDiv);
 });
 
-players = playerNames.map(createPlayer);
-players.forEach((player) => {
-    const playerDiv = createPlayerDiv(player);
-    playersDiv.appendChild(playerDiv);
-});
+setInterval(() => {
+    if (gameInProgress && !gamePaused) {
+        gameTime++;
+        updateGameTimeDisplay();
 
-updateGameHistory();
-
+        players.forEach((player, index) => {
+            if (player.playing && !player.benchStart) {
+                player.playTime++;
+            } else if (player.playing && player.benchStart) {
+                player.benchTime++;
+            }
+            updatePlayerDisplay(player, playersDiv.children[index]);
+        });
+    }
+}, 1000);
